f85d2c70068fae8cec28c183ef888012b62ddb1c
harshitaram <harshita@uchicago.edu>
2022-02-16 00:36:57 +0000

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+  get("/directors/youngest", { :controller => "directors", :action => "smallest" })
   get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:an_id", { :controller => "actors", :action => "actor_details" })
 end
